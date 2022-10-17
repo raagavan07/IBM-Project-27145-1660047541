1811c5955b26e0f9604ea1c32f6e5637b8f9a118
madhu4117 <107028859+madhu4117@users.noreply.github.com>
2022-10-17 10:51:51 +0530

    Uploading Assignments

---
+++ Assignments/TL - Raagavan J S/Assignment 1/action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Result</title>
+</head>
+<body style="position: absolute; top: 22%; left: 42%">
+    Welcome <?php echo $_GET["uname"]; ?><br>
+    Your Email Address is: <?php echo $_GET["mail"]; ?><br>
+    Your Password is : <?php echo $_GET["pass"]; ?><br>
+</body>
+</html>
